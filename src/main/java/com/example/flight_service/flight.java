69dcf42ee4e9c2c448a586dc8b1bc16ed8d3eba9
satyam pandey <satyampandey@satyams-MacBook-Pro.local>
2025-10-25 13:26:43 +0530

    hello

--- src/main/java/com/example/flight_service/flight.java
+++ src/main/java/com/example/flight_service/flight.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class flight {
+
     @GetMapping("/flight")
     public String getData() {
-        return "Please  book all your flight from makemytip at 20% discount";
+        return "Please book all your flights from Yatra at 20% discount!";
     }
 }
